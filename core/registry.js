497843c3f2c0ac601ac5e1a555def985a9507384
ALFACLS2 <anggoro.sasmito@sat.co.id>
2026-03-29 20:36:16 +0700

    Create registry.js

---
+++ core/registry.js
@@ -0,0 +1,9 @@
+// manual registry (versi aman tanpa bundler)
+
+import lowongan from "../modules/pages/lowongan/manifest.js";
+import faq from "../modules/pages/faq/manifest.js";
+
+export const registry = [
+  lowongan,
+  faq
+];
