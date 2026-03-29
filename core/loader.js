92d9dfb18009d44f4ded718905903c01fbc5335a
ALFACLS2 <anggoro.sasmito@sat.co.id>
2026-03-29 21:01:50 +0700

    Update loader.js

--- core/loader.js
+++ core/loader.js
@@ -1,23 +1,3 @@
-import { resolveRoute } from "./router.js";
-
-export async function initApp() {
-  const app = document.getElementById("app");
-
-  async function render() {
-    const path = window.location.pathname || "/";
-
-    const route = resolveRoute(path);
-
-    if (!route) {
-      app.innerHTML = "<h1>404</h1>";
-      return;
-    }
-
-    const module = await import(route.module);
-    module.mount(app);
-  }
-
-  window.addEventListener("popstate", render);
-
-  render();
+export async function loadModule(module) {
+  return module;
 }
