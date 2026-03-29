2dbf3a68d1675985e7d0706221795b1105cc5f2b
ALFACLS2 <anggoro.sasmito@sat.co.id>
2026-03-29 21:15:51 +0700

    Update index.js

--- modules/pages/faq/index.js
+++ modules/pages/faq/index.js
@@ -5,6 +5,10 @@ import { layout } from '../../../components/layouts/default.js';
 export function render() {
   const data = getFAQ();
 
-  document.getElementById('app').innerHTML =
-    layout(view(data));
+  const app = document.getElementById('app');
+  app.innerHTML = layout(view(data));
+
+  document.getElementById('backBtn').addEventListener('click', () => {
+    window.location.pathname = '/lowongan';
+  });
 }
