3e8903f0f02b20eeeb55cdf6c3b9dbb6c0e70c7e
ALFACLS2 <anggoro.sasmito@sat.co.id>
2026-03-29 21:15:38 +0700

    Update view.js

--- modules/pages/faq/view.js
+++ modules/pages/faq/view.js
@@ -1,13 +1,18 @@
 export function view(data) {
   return `
+    <div class="bg"></div>
+
     <div class="card">
       <h1>FAQ</h1>
+
       ${data.map(f => `
-        <div>
+        <div style="margin-bottom:10px;">
           <b>${f.q}</b>
           <p>${f.a}</p>
         </div>
       `).join('')}
+
+      <button id="backBtn" class="btn">← Back</button>
     </div>
   `;
 }
