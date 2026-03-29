baf200936f028c91a79dd521f0799d1bc3349364
ALFACLS2 <anggoro.sasmito@sat.co.id>
2026-03-29 20:50:00 +0700

    Update router.js

--- core/router.js
+++ core/router.js
@@ -3,3 +3,9 @@ import { registry } from "./registry.js";
 export function resolveRoute(path) {
   return registry.find(r => r.route === path);
 }
+const BASE = "/Proto2"; // ganti sama nama repo lo
+
+export function resolveRoute(path) {
+  const cleanPath = path.replace(BASE, "") || "/";
+  return registry.find(r => r.route === cleanPath);
+}
