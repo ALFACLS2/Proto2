6d1172a1ae447d4775a1c2aa6b4e33d4cef055c8
ALFACLS2 <anggoro.sasmito@sat.co.id>
2026-03-29 21:02:51 +0700

    Update manifest.js

--- modules/pages/lowongan/manifest.js
+++ modules/pages/lowongan/manifest.js
@@ -1,6 +1,6 @@
 export default {
-  name: "lowongan",
-  route: "/",
-  module: "/modules/pages/lowongan/index.js",
-  layout: "default"
+  name: 'lowongan',
+  route: '/lowongan',
+  layout: 'default',
+  entry: '../modules/pages/lowongan/index.js'
 };
