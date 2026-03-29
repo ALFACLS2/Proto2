154d5ca694fc0b4f58844f641c7a1ba3ea0f6055
ALFACLS2 <anggoro.sasmito@sat.co.id>
2026-03-29 21:03:25 +0700

    Update view.js

--- modules/pages/lowongan/view.js
+++ modules/pages/lowongan/view.js
@@ -1,10 +1,9 @@
-import { links } from "../../../data/links.js";
-
 export function view() {
   return `
   <div class="bg"></div>
 
   <div class="card">
+
     <div class="hero">
       <div class="hero-img"></div>
     </div>
@@ -14,20 +13,28 @@ export function view() {
       Ikuti langkah berikut untuk melamar kerja
     </div>
 
-    <a href="${links.register}" class="btn primary" target="_blank">
-      🚀 Register
+    <a href="https://alfakarir.alfamart.co.id/register" target="_blank" class="btn primary">
+      🚀 Step 1: Register Account
     </a>
 
-    <a href="${links.apply}" class="btn" target="_blank">
-      📄 Apply Job
+    <a href="https://alfakarir.alfamart.co.id/vacancy?filter=true&branch=cileungsi2&idb=ZlFwT3pUaVBqWmNQU2poOXVUazlnZz09" target="_blank" class="btn">
+      📄 Step 2: Apply Job
     </a>
 
     <div class="separator"></div>
 
     <div class="social">
-      <button id="faqBtn" class="btn">❓ FAQ</button>
-      <a href="${links.tiktok}" target="_blank">TikTok</a>
+      <button class="btn" id="faqBtn">❓ FAQ</button>
+
+      <a href="https://www.tiktok.com" target="_blank">
+        TikTok
+      </a>
     </div>
+
+    <div class="footer">
+      <div>Powered by <strong>REC CLS 2</strong></div>
+    </div>
+
   </div>
   `;
 }
